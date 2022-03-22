9b26aa3afda6d52e839dda618c6a4ce09c4f86fb
Jonahowns <jprocyk@asu.edu>
2022-03-22 10:09:03 -0700

    added pig_np3_c2 hyperparam optimization script

---
+++ rbm_torch/agave_hyperparam_submit/pig_np3_c2_hyperparam_optim.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH -A jprocyk
+#SBATCH -n 1                  # number of cores
+#SBATCH -c 36
+#SBATCH -t 0-04:00               # wall time (D-HH:MM)
+#SBATCH -o slurm_rbm_hyperparam_hidden_opt_cov_r1.%j.out          # STDOUT (%j = JobId)
+#SBATCH -e slurm.%j.err          # STDERR (%j = JobId)
+#SBATCH -p htcgpu                            #sulcgpu2
+#SBATCH -q normal                            #sulcgpu1
+#SBATCH -C A100
+#SBATCH --gres=gpu:4
+#SBATCH --mail-type=BEGIN,END,FAIL     # notifications for job done & fail
+#SBATCH --mail-user=jprocyk@asu.edu     #send to my email
+#SBATCH --chdir=/scratch/jprocyk/machine_learning/phage_display_ML/rbm_torch/
+
+source activate exmachina3
+
+python rbm_hyperparam_optimization.py /scratch/jprocyk/machine_learning/phage_display_ML/pig_tissue/np3_c2.fasta 45 protein 3 100 1 12 6 False
+
+# python rbm_hyperparam_optimization.py DATASET_PATH VISIBLE_NUM MOLECULE SAMPLES EPOCHS GPUS CPUS DATAWORKERS WEIGHTS
