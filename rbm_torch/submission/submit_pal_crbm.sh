12d072fb83ad1a8ee240eb7696c42fe3204e9017
Jonahowns <jprocyk@asu.edu>
2022-06-02 14:59:27 -0700

    Added pal data

---
+++ rbm_torch/submission/submit_pal_crbm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sbatch pal_crbm_r3.sh 
+sbatch pal_crbm_r5.sh 
+sbatch pal_crbm_r7.sh 
+sbatch pal_crbm_r8.sh 
+sbatch pal_crbm_r9.sh 
+sbatch pal_crbm_r10.sh 
+sbatch pal_crbm_r11.sh 
+sbatch pal_crbm_r12.sh 
+sbatch pal_crbm_r13.sh 
+sbatch pal_crbm_r14.sh 
+sbatch pal_crbm_r15.sh 
+sbatch pal_crbm_r16.sh 
