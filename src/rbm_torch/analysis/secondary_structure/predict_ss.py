6286b4737a271dce03862afff0d6b9cf1a7e17f6
Jonahowns <jprocyk@asu.edu>
2022-08-04 13:32:45 -0700

    changed epoch number in crbm and rbm for cov comp dataset

--- src/rbm_torch/analysis/secondary_structure/predict_ss.py
+++ src/rbm_torch/analysis/secondary_structure/predict_ss.py
@@ -1,25 +1,29 @@
-import nupack as nu
-import sys
+from nupack import *
+
+config.parallelism = True
+config.cache = 8.0 # GB
+
+
 sys.path.append("../../../")
 from utils import fasta_read
 import math
 
 # sodium range [0.05,1.1]
 # magnesium range [0.0, 0.2]
-def nupack_predict_ss(fasta_file, molecule="dna", celsius=23, sodium=0.5, magnesium=0.01):
+def nupack_predict_ss(fasta_file, molecule="dna", ensemble="stacking", celsius=23, sodium=0.05, magnesium=0.012):
     assert molecule in ["dna", "rna"]
     all_seqs, all_counts, all_chars, q = fasta_read(fasta_file, molecule, threads=6, drop_duplicates=True)
     no_wildcards = [seq for seq in all_seqs if "N" not in seq and "-" not in seq]  # Must remove all wildcard sequences or nupack won't work
 
-    model = nu.Model(material=molecule.upper(), celsius=celsius, sodium=sodium, magnesium=magnesium)
+    model = Model(material=molecule.upper(), celsius=celsius, sodium=sodium, magnesium=magnesium)
     batch_size = 250
     total_batches = math.ceil(len(no_wildcards) / batch_size)
     secondary_structures = []
     for i in range(total_batches):
         if i == total_batches - 1:
-            mfe = nu.mfe(strands=no_wildcards[i * batch_size:], model=model)  # Calculate mfe for all provided sequences
+            mfe = mfe(strands=no_wildcards[i * batch_size:], model=model)  # Calculate mfe for all provided sequences
         else:
-            mfe = nu.mfe(strands=no_wildcards[i * batch_size:(i + 1)*batch_size], model=model)  # Calculate mfe for all provided sequences
+            mfe = mfe(strands=no_wildcards[i * batch_size:(i + 1)*batch_size], model=model)  # Calculate mfe for all provided sequences
         secondary_structures += [str(x.structure) for x in mfe]
     return no_wildcards, secondary_structures
 
