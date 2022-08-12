905c1cc5d0c0abdd3b10d2ea04a311751e3f8d60
Jonahowns <jprocyk@asu.edu>
2022-08-11 20:00:33 -0600

    fixed small error in code, I think it should maybe???? not crash. I will pray and perform my daily human sacrifices to the pytorch gods

--- train.py
+++ train.py
@@ -91,5 +91,5 @@ if __name__ == '__main__':
         # distributed data parallel, multi-gpus on single machine or across multiple machines
         plt = Trainer(max_epochs=config['epochs'], logger=logger, gpus=run_data["gpus"], accelerator="ddp")  # distributed data-parallel
     else:
-        plt = Trainer(max_epochs=config['epochs'], logger=logger, gpus=run_data["gpus"])  # gpus=1,
+        plt = Trainer(max_epochs=config['epochs'], logger=logger, devices=run_data["gpus"], accelerator="gpu")  # gpus=1,
     plt.fit(model)
