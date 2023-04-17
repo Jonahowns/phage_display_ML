ff370d332ea9aceb49603c497a5335eca9f38d52
Jonahowns <jprocyk@asu.edu>
2023-04-16 23:45:04 -0400

    added submission scripts for exosome dataset

---
+++ datasets/exo/submission/pcrbm_cluster_pcrbm_cluster_exo_fe.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH -A jprocyk
+#SBATCH -n 1
+#SBATCH -c 6
+#SBATCH -t 7-00:00
+#SBATCH -p wzhengpu1
+#SBATCH -q wildfire
+#SBATCH --gres=gpu:1
+#SBATCH -o ./datasets/exo/submission/out/pcrbm_cluster_exo_fe.%j.out
+#SBATCH -e ./datasets/exo/submission/err/slurm.%j.err
+#SBATCH --mail-type=BEGIN,END,FAIL     # notifications for job done & fail
+#SBATCH --mail-user=jprocyk@asu.edu     #send to my email
+#SBATCH --chdir=/scratch/jprocyk/machine_learning/phage_display_ML/
+
+source activate aptamer
+
+python train.py ./datasets/exo/run_files/exo_pcrbm_cluster_fe.json
+
